dc391ad53ad1f0653b858a8e06a3aad28c43b916
Howard_Holcombe <sixkids7@earthlink.net>
2014-03-25 21:31:29 -0400

    after lab #5

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Book.create(title:'The Hobbit', author:'tolkien', pages:384, price:22.68)
+Book.create(title:'Atlas Shrugged', author:'Ayn rand', pages:1200, price:18.81)
+Book.create(title:"ender's game", author:'Orson Scott Card', pages:352, price:4.83)
+Book.create(title:'Hamlet', author:'Shakespeare', pages:342, price:5.34)
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
