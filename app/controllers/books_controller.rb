25bf9f69cbd0b6f6dfc1d5d51753f8842e9af42c
Howard_Holcombe <sixkids7@earthlink.net>
2014-03-25 21:18:17 -0400

    at end of helper section

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
    def index
+   	  @available_at = Time.now
+   	  @books = [ "Atlas Shrugged", "Ender's game", "Hamlet", "The hobbit"]
    end
    end
