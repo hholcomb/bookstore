945767ca40bf0fde36d6d929375c7e57c3f82450
Howard_Holcombe <sixkids7@earthlink.net>
2014-04-01 21:26:33 -0400

    lecture 9 - end of lab #4

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,16 @@ class BooksController < ApplicationController
    def show
    	 @book = Book.find(params[:id])
    	end
+  
+
+   def edit
+   	@book = Book.find(params[:id])
    end
+
+   def update
+   	 @book = Book.find(params[:id])
+   	 @book.update(book_params)
+   	 redirect_to @book
+   end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ get "/books" => "books#index", as: 'books'
 get "/books/new" => "books#new", as: 'new_book'
 get "/books/:id" => "books#show", as: 'book'
 post "/books" => "books#create"
+get "/books/:id/edit" => "books#edit", as: 'edit_book'
+patch "/books/:id" => "books#update"
 
 
 
