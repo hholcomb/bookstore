7f6dca23eb59f05154923dfd7b5a23da0218f95f
Howard_Holcombe <sixkids7@earthlink.net>
2014-04-01 20:40:52 -0400

    lecture 9 - end of lab #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,20 @@
 class ApplicationController < ActionController::Base
+
+	def new
+	  @book = Book.new
+	 end
+
+	 def create
+	   @book = Book.new(book_params)
+	   @book.save
+	   redirect_to @book
+	  end
+
+	  private
+
+	  def book_params
+	  	params.require(:book).permit(:title, :author, :pages, :price)
+	  end
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
