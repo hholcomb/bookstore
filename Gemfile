509dc1c1ca4b28c9446d7479a0b07e18b988bdee
Howard Holcombe <sixkids7@earthlink.net>
2014-04-15 20:22:36 -0400

    lesson 11 - lab #5

--- Gemfile
+++ Gemfile
@@ -32,6 +32,9 @@ gem 'jbuilder', '~> 1.2'
 gem 'kaminari'
 gem 'kaminari-bootstrap', '~> 3.0.1'
 
+gem 'powerpack'
+
+
 
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Bookstore::Application.routes.draw do
+  resources :reviews
+
 resources :books do 
+  resources :reviews
   get 'page/:page', :action => :index, :on => :collection
 end
-  
+
 root 'books#index'
 
 
